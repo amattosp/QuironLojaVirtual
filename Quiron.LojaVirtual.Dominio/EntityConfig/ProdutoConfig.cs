212a6a1ec2880104fa44c6f17bf29823686f14d6
amattosp <ander_mattos@yahoo.com.br>
2015-10-24 03:48:54 +0000

    Quiron Loja Vritual - Implementando classe DBContext com Migrations

---
+++ Quiron.LojaVirtual.Dominio/EntityConfig/ProdutoConfig.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Quiron.LojaVirtual.Dominio.Entidades;
+
+namespace Quiron.LojaVirtual.Dominio.EntityConfig
+{
+    public class ProdutoConfig : EntityTypeConfiguration<Produto>
+    {
+        public ProdutoConfig()
+        {
+            HasKey(c => c.ProdutoId);
+
+            Property(c => c.Nome)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            Property(c => c.Descricao)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            
+            Property(c => c.Preco)
+                .IsRequired();
+            
+            Property(c => c.Categoria)
+                .IsRequired()
+                .HasMaxLength(60);
+
+
+            ToTable("Produtos");
+        }
+    }
+}
---
+++ Quiron.LojaVirtual.Dominio/Repositorio/EfDBContext.cs
@@ -0,0 +1,65 @@
+﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using Quiron.LojaVirtual.Dominio.Entidades;
+using Quiron.LojaVirtual.Dominio.EntityConfig;
+namespace Quiron.LojaVirtual.Dominio.Repositorio
+{
+    public class EfDBContext : DbContext 
+    {
+
+        public EfDBContext()
+            : base("QuironLojaContext")
+        {
+
+        }
+    
+    
+        public DbSet<Produto> Produtos { get; set; }
+        
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // General Custom Context Properties
+            modelBuilder.Properties()
+                .Where(p => p.Name == p.ReflectedType.Name + "Id")
+                .Configure(p => p.IsKey());
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasColumnType("varchar"));
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p.HasMaxLength(100));
+
+            modelBuilder.Configurations.Add(new ProdutoConfig());
+          
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            //foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            //{
+            //    if (entry.State == EntityState.Added)
+            //    {
+            //        entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+            //    }
+
+            //    if (entry.State == EntityState.Modified)
+            //    {
+            //        entry.Property("DataCadastro").IsModified = false;
+            //    }
+            //}
+            return base.SaveChanges();
+        }
+    }
+
+
+    
+    
+    
+    
+    }
+
